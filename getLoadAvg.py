da4d27ba235a757f79d189029319f48782e1825e
Jatin Rungta <jatinrungta27@gmail.com>
2017-03-31 23:03:13 +0530

    Remove Hardcodes
    *Cleaned code
    *Server can now run on any pc

--- getLoadAvg.py
+++ getLoadAvg.py
@@ -1,7 +1,4 @@
-import time
 import socket
-import sys
-import os
 import operator
 import threading
 
@@ -10,49 +7,43 @@ new_dict = {}
 class Client(threading.Thread):
 	lock = threading.Lock()
 
-	def __init__(self,connection,client_address): 
-		threading.Thread.__init__(self) 
+	def __init__(self,connection,clientAddress):
+		threading.Thread.__init__(self)
 		self.conn = connection
-		self.clientaddress = client_address
- 
-	def run(self): 
-		(ipofclient,port) = self.clientaddress
+		self.clientAddress = clientAddress
+
+	def run(self):
+		(ipOfClient,port) = self.clientAddress
 		
 		while True:
-			filename = open("/home/jarvis/Documents/Docker/out.txt","w")
+			filename = open("/tmp/iplist.txt","w")
 			data = self.conn.recv(10)
-			if str(data) == "b''":
-				data = "100"
-				new_dict[ipofclient] = data
-				Client.lock.acquire()
-				print(len(new_dict))
-				sorted_dict = sorted(new_dict.items(), key=operator.itemgetter(1))
-				for key,value in sorted_dict:
-					filename.write(key + " " + value + "\n")
-				Client.lock.release()
-				break
-
 			data = str(data)
-			data = data[2:-1]
-			print(ipofclient + " " + data)
-			
-			new_dict[ipofclient]=data
+
+			if data == "b''":
+				data = "100"
+				break
+			else:
+				data = data[2:-1]
+
+			print(ipOfClient + " " + data)
+
+			new_dict[ipOfClient]=data
 
 			Client.lock.acquire()
 			print(len(new_dict))
 			sorted_dict = sorted(new_dict.items(), key=operator.itemgetter(1))
 			for key,value in sorted_dict:
 				filename.write(key + " " + value + "\n")
-			Client.lock.release()	
-		
+			Client.lock.release()
+
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server_address=("172.31.132.88",9994)
-sock.bind(server_address)
+serverAddress=('',9994)
+sock.bind(serverAddress)
 sock.listen(10)
 
 while True:
 	print("Waiting for Connections")
-	connection,client_address = sock.accept()
-	thread = Client(connection,client_address)		
+	connection,clientAddress = sock.accept()
+	thread = Client(connection,clientAddress)
 	thread.start()
-	
