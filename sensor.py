71de05aad2bb5e881bcf2210b8c8e921f6d47bf8
Jatin Rungta <jatinrungta27@gmail.com>
2017-04-04 00:00:42 +0530

    Fix sensors

--- sensor.py
+++ sensor.py
@@ -4,22 +4,24 @@ import sys
 import psutil
 import random
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Create a gTCP/IP socket
+
 
 # Connect the socket to the port where the server is listening
-server_address = ("172.31.132.220", 10003)
+server_address = ("172.31.132.89", 10003)
 #print >> sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
+
 
 try:
 	while(True):
-	    message = random.randint(0,99)
-	    message = str(message)
-	    print(message)
-	    '''print >> sys.stderr, 'sending "%s"' % message'''
-	    sock.sendall(message.encode())
-	    time.sleep(1)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect(server_address)
+		message = random.randint(0,99)
+		message = str(message)
+		print(message)
+		sock.sendall(message.encode())
+		time.sleep(1)
+		sock.close()
 
 finally:
     #print >> sys.stderr, 'closing socket'
