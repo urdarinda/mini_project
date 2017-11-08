f2a20f369384aea85c7caac782f877a2b3496ffd
Jatin Rungta <jatinrungta27@gmail.com>
2017-11-08 13:50:07 +0530

    Proper peer printing

--- interface.py
+++ interface.py
@@ -1,33 +1,34 @@
 from imagedht import ImageDHT
 import sys
 
-dht_object = ImageDHT(sys.argv[1],int(sys.argv[2]),(sys.argv[3],int(sys.argv[4])))
+dht_object = ImageDHT(sys.argv[1], int(sys.argv[2]),
+                      (sys.argv[3], int(sys.argv[4])))
+
 
 while True:
-	
-	query = input("Enter your query\n")
-	query = int(query)
 
-	if query == 1:
-		key = input("Enter your key\n")
-		dht_object.get_item_iptocpu(key)
+    query = int(input("Enter your query :"))
 
-	elif query == 2:
-		key = input("Enter your key\n")
-		dht_object.get_item_exttoip(key)
+    if query == 1:
+        key = input("Enter your key :")
+        dht_object.get_iptocpu(key)
 
-	elif query == 3:
-		dht_object.get_peers()
+    elif query == 2:
+        key = input("Enter your key :")
+        dht_object.get_exttoip(key)
 
-	elif query == 4:
-		dht_object.get_all_keys_iptocpu()
+    elif query == 3:
+        dht_object.get_peers()
 
-	elif query == 5:
-		dht_object.get_all_keys_exttoip()
-	
-	elif query == 6:
-		print("Bye Bye\n")
-		exit()
+    elif query == 4:
+        dht_object.get_all_iptocpu()
 
-	else:
-		print("Invalid Input\n")
+    elif query == 5:
+        dht_object.get_all_exttoip()
+
+    elif query == 6:
+        print("Bye Bye\n")
+        exit()
+
+    else:
+        print("Invalid Input\n")
