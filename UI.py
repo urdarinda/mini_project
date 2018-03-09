86093fecaf2d6b4979aa78d67b9e22ed71c8315b
Deepak <firstacceptedcode@gmail.com>
2018-03-09 23:11:52 +0530

    Add files via upload

---
+++ UI.py
@@ -0,0 +1,87 @@
+import sys
+import code
+import code2
+#import interface
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'Data about DHT'
+        self.left = 20
+        self.top = 20
+        self.width = 640
+        self.height = 480
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        #Object = interface.UIHandler(argv1,argv2,argv3,argv3)
+
+
+        #######
+        self.textbox_one = QLineEdit(self)
+        self.textbox_one.move(20,20)
+        self.textbox_one.resize(220,27)
+
+        self.button_one = QPushButton('Get the CPU usage of this node', self)
+        self.button_one.move(280, 20)
+        self.button_one.resize(250,self.button_one.sizeHint().height())
+        self.button_one.clicked.connect(self.on_click_one)
+
+
+        #######
+        self.textbox_two = QLineEdit(self)
+        self.textbox_two.move(20, 80)
+        self.textbox_two.resize(220, 27)
+
+        self.button_two = QPushButton('Get the list of nodes having this image', self)
+        self.button_two.move(280, 80)
+        self.button_two.resize(250, self.button_two.sizeHint().height())
+        self.button_two.clicked.connect(self.on_click_two)
+
+
+        #######
+        self.button_three = QPushButton('Get the list of all the peers present in DHT', self)
+        self.button_three.move(20, 140)
+        self.button_three.resize(500, self.button_three.sizeHint().height())
+        self.button_three.clicked.connect(self.on_click_three)
+
+        self.show()
+
+    def on_click_one(self):
+
+        self.textboxvalue = self.textbox_one.text()
+        #self.textboxvalue = self.Object.extract_info(1,self.textboxvalue)
+        self.textboxvalue = code.func(self.textboxvalue)
+        QMessageBox.question(self,'Information',self.textboxvalue,QMessageBox.Ok,QMessageBox.Ok)
+        self.textbox_one.setText("")
+        #self.textbox.text("")
+
+    def on_click_two(self):
+        self.textboxvalue = self.textbox_two.text()
+        # self.textboxvalue = self.Object.extract_info(2,self.textboxvalue)
+        self.textboxvalue = code.func(self.textboxvalue)
+        QMessageBox.question(self,'Information',self.textboxvalue,QMessageBox.Ok,QMessageBox.Ok)
+        self.textbox_two.setText("")
+        #self.textbox.text("")
+
+    def on_click_three(self):
+        obj = code2.MyClass()
+        self.textboxvalue = obj.func()
+        # self.textboxvalue = self.Object.extract_info(3,"")
+        QMessageBox.question(self,'Information',self.textboxvalue,QMessageBox.Ok,QMessageBox.Ok)
+        #self.textbox_one.setText("")
+        #self.textbox_two.setText("")
+        #self.textbox.text("")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
---
+++ new_interface.py
@@ -0,0 +1,39 @@
+'''Interface for the system
+This file provides a way to get information about the DHT.
+a)The cpu usage of a particular node in DHT
+b)The list of nodes having a particular image in DHT
+c)list all the peers
+'''
+from imagedht import ImageDHT
+import sys
+
+'''
+four arguments are your ip, your port, the ip of the node you are connecting to and its port.
+'''
+
+class UIHandler:
+
+    def __init__(self,argv1,argv2,argv3,argv4):
+        self.dht_object = ImageDHT(argv1, int(argv2), argv3, int(argv4))
+
+    def extract_info(self,number,query):
+        number = int(number)
+
+        if number == 1:
+            # enter the ip as key to get its cpu usage in the DHT
+            return self.dht_object.get_iptocpu(query)
+
+        elif number == 2:
+            # enter the name of the image as the key to get the list of the nodes having the particular image in DHT
+            return self.dht_object.get_exttoip(query)
+
+        elif number == 3:  # this prints the list of the peers present in the DHT
+            return self.dht_object.get_peers()
+
+        '''
+        elif query == 4:  # get all the nodes in the ip_to_cpu DHT
+            dht_object.get_all_iptocpu()
+
+        elif query == 5:  # get all the nodes in the ext_to_ip DHT
+            dht_object.get_all_exttoip()
+        '''
